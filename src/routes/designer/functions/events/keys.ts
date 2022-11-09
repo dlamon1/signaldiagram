5a039f3c592ef5eb9deaa3e6626dbe197ac62f0c
Devon Lamond <devon.lamond@gmail.com>
2022-11-09 17:14:23 -0500

    bug isWindows

--- src/routes/designer/functions/events/keys.ts
+++ src/routes/designer/functions/events/keys.ts
@@ -1,61 +1,53 @@
-import { get } from "svelte/store";
+import { get } from 'svelte/store';
 
-import {isWindows} from 'is-windows'
+import pkg from 'is-windows';
+const isWindows = pkg;
 
-import {
-  isShifted,
-  setIsShifted,
-  isCtrl,
-  setIsCtrl,
-  setMode,
-  isSelectMode,
-} from "../../store";
+import { isShifted, setIsShifted, isCtrl, setIsCtrl, setMode, isSelectMode } from '../../store';
 
 console.log(isWindows);
 
-
-let isMac = !isWindows
-
+let isMac = !isWindows;
 
 export const handleKeyDown = (e: any) => {
-  if (e.keyCode === 16 && !get(isShifted)) {
-    setIsShifted(true);
-  }
+	if (e.keyCode === 16 && !get(isShifted)) {
+		setIsShifted(true);
+	}
 
-  if (e.keyCode === 16 && get(isShifted) && get(isSelectMode)) {
-    const body = document.getElementById("canvas-wrapper");
-    if (!body) return;
-    body.style.cursor = "crosshair";
-  }
+	if (e.keyCode === 16 && get(isShifted) && get(isSelectMode)) {
+		const body = document.getElementById('canvas-wrapper');
+		if (!body) return;
+		body.style.cursor = 'crosshair';
+	}
 
-  // if ctrl is pressed, set isCtrl to true
-  if (e.keyCode === 17 && !get(isCtrl) && !isMac && get(isSelectMode)) {
-    setIsCtrl(true);
-  }
-  // if command on mac is pressed
-  if (e.keyCode === 91 && isMac && get(isSelectMode)) {
-    setIsCtrl(true);
-  }
+	// if ctrl is pressed, set isCtrl to true
+	if (e.keyCode === 17 && !get(isCtrl) && !isMac && get(isSelectMode)) {
+		setIsCtrl(true);
+	}
+	// if command on mac is pressed
+	if (e.keyCode === 91 && isMac && get(isSelectMode)) {
+		setIsCtrl(true);
+	}
 
-  if (e.keyCode === 68 && get(isShifted)) {
-    setMode("draw");
-  }
-  if (e.keyCode === 83 && get(isShifted)) {
-    setMode("select");
-  }
+	if (e.keyCode === 68 && get(isShifted)) {
+		setMode('draw');
+	}
+	if (e.keyCode === 83 && get(isShifted)) {
+		setMode('select');
+	}
 };
 
 export const handleKeyUp = (e: any) => {
-  if (e.keyCode === 16) {
-    setIsShifted(false);
-    const body = document.getElementById("canvas-wrapper");
-    if (!body) return;
-    body.style.cursor = "default";
-  }
-  if (e.keyCode === 17 && get(isCtrl) && !isMac) {
-    setIsCtrl(false);
-  }
-  if (e.keyCode === 91 && isMac) {
-    setIsCtrl(false);
-  }
+	if (e.keyCode === 16) {
+		setIsShifted(false);
+		const body = document.getElementById('canvas-wrapper');
+		if (!body) return;
+		body.style.cursor = 'default';
+	}
+	if (e.keyCode === 17 && get(isCtrl) && !isMac) {
+		setIsCtrl(false);
+	}
+	if (e.keyCode === 91 && isMac) {
+		setIsCtrl(false);
+	}
 };
