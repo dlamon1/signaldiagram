e279a59f4650a3c0e02266c9386fa6a81ac51667
Devon Lamond <devon.lamond@gmail.com>
2022-11-10 13:25:37 -0500

    add links

--- src/routes/designer/InfoToolbar/InfoBar.Links.svelte
+++ src/routes/designer/InfoToolbar/InfoBar.Links.svelte
@@ -1,56 +1,47 @@
 <script lang="ts">
-  import { tooltip } from "../Tooltips/tooltip";
+	import { tooltip } from '../Tooltips/tooltip';
 
-  import { isExportDialogOpen } from "../store";
+	import { isExportDialogOpen } from '../store';
 </script>
 
-<button class="dialog" on:click={() => ($isExportDialogOpen = true)}
-  >Import / Export</button
->
+<button class="dialog" on:click={() => ($isExportDialogOpen = true)}>Import / Export</button>
 
 <div class="link-wrapper">
-  <a
-    title="Thank you for the help!"
-    use:tooltip
-    href="https://github.com/dlamon1/signaldiagram/issues"
-    target="_blank"
-  >
-    Report a Bug</a
-  >
+	<a href="/add" target="_blank"> Add a Tile Type</a>
 </div>
 
 <div class="link-wrapper">
-  <a href="mailto: support@leadled.io">Feedback</a>
+	<a
+		title="Thank you for the help!"
+		use:tooltip
+		href="https://github.com/dlamon1/signaldiagram/issues"
+		target="_blank"
+	>
+		Report a Bug</a
+	>
 </div>
 
 <div class="link-wrapper">
-  <a
-    class="version"
-    title="Click here for Version 1.0.0"
-    use:tooltip
-    href="https://v1.signaldiagram.com"
-  >
-    Version 2.2.1</a
-  >
+	<a href="mailto: support@leadled.io">Feedback</a>
 </div>
 
 <style>
-  .version {
-    margin-top: 10px;
-    align-self: center;
-    color: aqua;
-  }
+	.version {
+		margin-top: 10px;
+		align-self: center;
+		color: aqua;
+	}
 
-  .dialog {
-    height: 40px;
-  }
+	.dialog {
+		height: 40px;
+	}
 
-  a {
-    color: #fff;
-    text-decoration: none;
-  }
-  .link-wrapper {
-    margin-top: 10px;
-    align-self: center;
-  }
+	a {
+		color: #fff;
+		text-decoration: none;
+	}
+	.link-wrapper {
+		margin-top: 10px;
+		align-self: center;
+	}
 </style>
