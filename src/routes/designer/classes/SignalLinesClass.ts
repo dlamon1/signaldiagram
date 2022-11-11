7ded522720986d32b16b73a7227d43556b4ea193
Devon Lamond <devon.lamond@gmail.com>
2022-11-10 21:09:39 -0500

    cable length rounding

--- src/routes/designer/classes/SignalLinesClass.ts
+++ src/routes/designer/classes/SignalLinesClass.ts
@@ -1,363 +1,340 @@
-import { get } from "svelte/store";
+import { get } from 'svelte/store';
 
 import {
-  colorState,
-  isCtrl,
-  setSelection,
-  setSignalLineColor,
-  currentScreenIndex,
-  screens,
-  updateScreens,
-} from "../store";
+	colorState,
+	isCtrl,
+	setSelection,
+	setSignalLineColor,
+	currentScreenIndex,
+	screens,
+	updateScreens
+} from '../store';
 
 import type {
-  LoadSignalLineObj,
-  SignalLineObj,
-  SignalLinesType,
-  SnapPointCoordinates,
-  SnapPointCoordinatesKey,
-  XYCoordinates,
-  ColorObjKey,
-  PanelObj,
-  SnapPointObj,
-} from "../Types/ClassTypes";
+	LoadSignalLineObj,
+	SignalLineObj,
+	SignalLinesType,
+	SnapPointCoordinates,
+	SnapPointCoordinatesKey,
+	XYCoordinates,
+	ColorObjKey,
+	PanelObj,
+	SnapPointObj
+} from '../Types/ClassTypes';
 
 export class SignalLines implements SignalLinesType {
-  array = [];
+	array = [];
 
-  origin = {
-    snapPointIndex: null,
-    panelIndex: null,
-    pointIndexWithinPanel: null,
-  };
+	origin = {
+		snapPointIndex: null,
+		panelIndex: null,
+		pointIndexWithinPanel: null
+	};
 
-  destination = {
-    snapPointIndex: null,
-    panelIndex: null,
-    pointIndexWithinPanel: null,
-  };
+	destination = {
+		snapPointIndex: null,
+		panelIndex: null,
+		pointIndexWithinPanel: null
+	};
 
-  mouse = {
-    x: null,
-    y: null,
-  };
+	mouse = {
+		x: null,
+		y: null
+	};
 
-  screenIndex: number;
+	screenIndex: number;
 
-  constructor(screenIndex: number) {
-    this.screenIndex = screenIndex;
-  }
+	constructor(screenIndex: number) {
+		this.screenIndex = screenIndex;
+	}
 
-  setArrayFromLoad(signaLineArray: LoadSignalLineObj[]) {
-    // this.array = [];
-    // signaLineArray.forEach((signalLine, i) => {
-    //   const newLine = new SignalLine(
-    //     signalLine.origin,
-    //     signalLine.destination.snapPointIndex
-    //   );
-    //   this.array.push(newLine);
-    //   const thisLine = this.array[i];
-    //   thisLine.updateColor(signalLine.color.background);
-    // });
-  }
+	setArrayFromLoad(signaLineArray: LoadSignalLineObj[]) {
+		// this.array = [];
+		// signaLineArray.forEach((signalLine, i) => {
+		//   const newLine = new SignalLine(
+		//     signalLine.origin,
+		//     signalLine.destination.snapPointIndex
+		//   );
+		//   this.array.push(newLine);
+		//   const thisLine = this.array[i];
+		//   thisLine.updateColor(signalLine.color.background);
+		// });
+	}
 
-  load(signaLineArray) {
-    this.array = [];
-    signaLineArray.forEach((signalLine, i) => {
-      const newLine = new SignalLine(
-        signalLine.origin,
-        signalLine.destination.snapPointIndex,
-        this.screenIndex
-      );
+	load(signaLineArray) {
+		this.array = [];
+		signaLineArray.forEach((signalLine, i) => {
+			const newLine = new SignalLine(
+				signalLine.origin,
+				signalLine.destination.snapPointIndex,
+				this.screenIndex
+			);
 
-      this.array.push(newLine);
-      const thisLine = this.array[i];
-      thisLine.updateColor(signalLine.color.background);
-    });
-  }
+			this.array.push(newLine);
+			const thisLine = this.array[i];
+			thisLine.updateColor(signalLine.color.background);
+		});
+	}
 
-  getSnapPointCoordinates(
-    signalLineIndex: number,
-    key: SnapPointCoordinatesKey
-  ) {
-    const signalLine = this.array[signalLineIndex];
-    const snapPointsClass = get(screens)[this.screenIndex].snapPoints;
+	getSnapPointCoordinates(signalLineIndex: number, key: SnapPointCoordinatesKey) {
+		const signalLine = this.array[signalLineIndex];
+		const snapPointsClass = get(screens)[this.screenIndex].snapPoints;
 
-    const snapPointIndex = signalLine[key].snapPointIndex;
+		const snapPointIndex = signalLine[key].snapPointIndex;
 
-    const snapPoint = snapPointsClass.array[snapPointIndex];
+		const snapPoint = snapPointsClass.array[snapPointIndex];
 
-    const snapPointX = snapPoint.getX();
-    const snapPointY = snapPoint.getY();
+		const snapPointX = snapPoint.getX();
+		const snapPointY = snapPoint.getY();
 
-    const x = snapPointX;
-    const y = snapPointY;
+		const x = snapPointX;
+		const y = snapPointY;
 
-    return {
-      x,
-      y,
-    };
-  }
+		return {
+			x,
+			y
+		};
+	}
 
-  getPanelIndex(row: number, column: number) {
-    const p = get(screens)[get(currentScreenIndex)].panels.array;
+	getPanelIndex(row: number, column: number) {
+		const p = get(screens)[get(currentScreenIndex)].panels.array;
 
-    for (let i = 0; i < p.length; i++) {
-      if (p[i].row === row && p[i].column === column) {
-        return i;
-      }
-    }
-  }
+		for (let i = 0; i < p.length; i++) {
+			if (p[i].row === row && p[i].column === column) {
+				return i;
+			}
+		}
+	}
 
-  nullDestinationSnapPointIndex() {
-    this.destination.snapPointIndex = null;
-  }
+	nullDestinationSnapPointIndex() {
+		this.destination.snapPointIndex = null;
+	}
 
-  setOriginSnapPointIndex(obj: SnapPointObj) {
-    const panelIndex = this.getPanelIndex(obj.row, obj.column);
+	setOriginSnapPointIndex(obj: SnapPointObj) {
+		const panelIndex = this.getPanelIndex(obj.row, obj.column);
 
-    const snapPointIndex = obj.pointIndexFullArray;
+		const snapPointIndex = obj.pointIndexFullArray;
 
-    this.origin.snapPointIndex = snapPointIndex;
-    this.origin.panelIndex = panelIndex;
-  }
+		this.origin.snapPointIndex = snapPointIndex;
+		this.origin.panelIndex = panelIndex;
+	}
 
-  nullOriginAndDestinationValues() {
-    this.origin.snapPointIndex = null;
-    this.origin.panelIndex = null;
-    this.destination.snapPointIndex = null;
-    this.destination.panelIndex = null;
-  }
+	nullOriginAndDestinationValues() {
+		this.origin.snapPointIndex = null;
+		this.origin.panelIndex = null;
+		this.destination.snapPointIndex = null;
+		this.destination.panelIndex = null;
+	}
 
-  setDestinationSnapPointIndex(snapPoint: SnapPointObj) {
-    this.destination.snapPointIndex = snapPoint.pointIndexFullArray;
-  }
+	setDestinationSnapPointIndex(snapPoint: SnapPointObj) {
+		this.destination.snapPointIndex = snapPoint.pointIndexFullArray;
+	}
 
-  setMousePosition(e: XYCoordinates) {
-    this.mouse.x = e.x;
-    this.mouse.y = e.y;
-  }
+	setMousePosition(e: XYCoordinates) {
+		this.mouse.x = e.x;
+		this.mouse.y = e.y;
+	}
 
-  addSignalLine() {
-    const origin = structuredClone(this.origin);
-    if (this.destination.snapPointIndex < 0) {
-      return;
-    }
+	addSignalLine() {
+		const origin = structuredClone(this.origin);
+		if (this.destination.snapPointIndex < 0) {
+			return;
+		}
 
-    if (this.origin.snapPointIndex != this.destination.snapPointIndex) {
-      const sl = new SignalLine(
-        origin,
-        this.destination.snapPointIndex,
-        this.screenIndex
-      );
+		if (this.origin.snapPointIndex != this.destination.snapPointIndex) {
+			const sl = new SignalLine(origin, this.destination.snapPointIndex, this.screenIndex);
 
-      this.array.push(sl);
+			this.array.push(sl);
 
-      updateScreens();
-    }
+			updateScreens();
+		}
 
-    this.nullOriginAndDestinationValues();
-  }
+		this.nullOriginAndDestinationValues();
+	}
 
-  removeSignalLine(line: SignalLineObj) {
-    this.array = this.array.filter((signalLine) => {
-      return signalLine.i !== line.i;
-    });
-  }
+	removeSignalLine(line: SignalLineObj) {
+		this.array = this.array.filter((signalLine) => {
+			return signalLine.i !== line.i;
+		});
+	}
 
-  selectSignalLine(i: number) {
-    const snapPointsClass = get(screens)[get(currentScreenIndex)].snapPoints;
-    const panelsClass = get(screens)[get(currentScreenIndex)].panels;
+	selectSignalLine(i: number) {
+		const snapPointsClass = get(screens)[get(currentScreenIndex)].snapPoints;
+		const panelsClass = get(screens)[get(currentScreenIndex)].panels;
 
-    snapPointsClass.deSelect();
-    panelsClass.deSelect();
+		snapPointsClass.deSelect();
+		panelsClass.deSelect();
 
-    const current = this.array[i].isSelected;
+		const current = this.array[i].isSelected;
 
-    if (!get(isCtrl)) {
-      this.array.forEach((sl) => sl.setIsSelected(false));
-      this.array[i].setIsSelected(true);
-    }
-    this.array[i].setIsSelected(true);
+		if (!get(isCtrl)) {
+			this.array.forEach((sl) => sl.setIsSelected(false));
+			this.array[i].setIsSelected(true);
+		}
+		this.array[i].setIsSelected(true);
 
-    setSelection("signallines");
-  }
+		setSelection('signallines');
+	}
 
-  toggleSignalLine(i: number) {
-    const snapPointsClass = get(screens)[get(currentScreenIndex)].snapPoints;
-    const panelsClass = get(screens)[get(currentScreenIndex)].panels;
+	toggleSignalLine(i: number) {
+		const snapPointsClass = get(screens)[get(currentScreenIndex)].snapPoints;
+		const panelsClass = get(screens)[get(currentScreenIndex)].panels;
 
-    snapPointsClass.deSelect();
-    panelsClass.deSelect();
+		snapPointsClass.deSelect();
+		panelsClass.deSelect();
 
-    const current = this.array[i].isSelected;
+		const current = this.array[i].isSelected;
 
-    if (!get(isCtrl)) {
-      this.array.forEach((sl) => sl.setIsSelected(false));
-      this.array[i].setIsSelected(!current);
-    }
-    this.array[i].setIsSelected(!current);
+		if (!get(isCtrl)) {
+			this.array.forEach((sl) => sl.setIsSelected(false));
+			this.array[i].setIsSelected(!current);
+		}
+		this.array[i].setIsSelected(!current);
 
-    setSelection("signallines");
-  }
+		setSelection('signallines');
+	}
 
-  selectSignalLines(arrayOfIndexes: number[]) {
-    const snapPointsClass = get(screens)[get(currentScreenIndex)].snapPoints;
-    const signalLinesClass = get(screens)[get(currentScreenIndex)].signalLines;
-    snapPointsClass.deSelect();
-    signalLinesClass.deSelect();
+	selectSignalLines(arrayOfIndexes: number[]) {
+		const snapPointsClass = get(screens)[get(currentScreenIndex)].snapPoints;
+		const signalLinesClass = get(screens)[get(currentScreenIndex)].signalLines;
+		snapPointsClass.deSelect();
+		signalLinesClass.deSelect();
 
-    if (!get(isCtrl)) {
-      this.array.forEach((line) => {
-        line.setIsSelected(false);
-      });
-    }
+		if (!get(isCtrl)) {
+			this.array.forEach((line) => {
+				line.setIsSelected(false);
+			});
+		}
 
-    arrayOfIndexes.forEach((i) => {
-      this.array[i].setIsSelected(true);
-    });
-  }
+		arrayOfIndexes.forEach((i) => {
+			this.array[i].setIsSelected(true);
+		});
+	}
 
-  deSelect() {
-    this.array.forEach((signalLine) => {
-      signalLine.setIsSelected(false);
-    });
-  }
+	deSelect() {
+		this.array.forEach((signalLine) => {
+			signalLine.setIsSelected(false);
+		});
+	}
 
-  setColors(key: ColorObjKey, color: string) {
-    let isSignalLinesSelected = false;
-    this.array.forEach((snapPoint) => {
-      if (snapPoint.isSelected) {
-        snapPoint.setColor(key, color);
-        isSignalLinesSelected = true;
-      }
-    });
-    if (!isSignalLinesSelected) {
-      setSignalLineColor(color);
-    }
-    updateScreens();
-  }
+	setColors(key: ColorObjKey, color: string) {
+		let isSignalLinesSelected = false;
+		this.array.forEach((snapPoint) => {
+			if (snapPoint.isSelected) {
+				snapPoint.setColor(key, color);
+				isSignalLinesSelected = true;
+			}
+		});
+		if (!isSignalLinesSelected) {
+			setSignalLineColor(color);
+		}
+		updateScreens();
+	}
 }
 
 class SignalLine implements SignalLineObj {
-  origin = {
-    snapPointIndex: 0,
-    panelIndex: 0,
-    pointIndexWithinPanel: 0,
-  };
-  destination = {
-    snapPointIndex: 0,
-    panelIndex: 0,
-    pointIndexWithinPanel: 0,
-  };
-  color = {
-    background: "#000000",
-    border: "#000000",
-    font: "#ffffff",
-  };
-  i: number;
-  lineWidth = 8;
-  isSelected = false;
-  screenIndex: number;
+	origin = {
+		snapPointIndex: 0,
+		panelIndex: 0,
+		pointIndexWithinPanel: 0
+	};
+	destination = {
+		snapPointIndex: 0,
+		panelIndex: 0,
+		pointIndexWithinPanel: 0
+	};
+	color = {
+		background: '#000000',
+		border: '#000000',
+		font: '#ffffff'
+	};
+	i: number;
+	lineWidth = 8;
+	isSelected = false;
+	screenIndex: number;
 
-  constructor(
-    origin: SnapPointCoordinates,
-    destinationSnapPointIndex: number,
-    screenIndex: number
-  ) {
-    this.screenIndex = screenIndex;
-    let screen = get(screens)[screenIndex];
-    this.origin.snapPointIndex = origin.snapPointIndex;
-    this.origin.panelIndex = origin.panelIndex;
-    this.destination.snapPointIndex = destinationSnapPointIndex;
-    this.i = get(screens)[screenIndex].signalLines.array.length;
-    this.color.background = get(colorState).signalLine.background;
-    this.lineWidth =
-      screen.width < screen.height ? screen.width / 20 : screen.height / 20;
-    this.setDestinationPanelIndex();
-    this.setIndexesWithInPanel();
-    // console.log(origin, destinationSnapPointIndex);
-  }
+	constructor(
+		origin: SnapPointCoordinates,
+		destinationSnapPointIndex: number,
+		screenIndex: number
+	) {
+		this.screenIndex = screenIndex;
+		let screen = get(screens)[screenIndex];
+		this.origin.snapPointIndex = origin.snapPointIndex;
+		this.origin.panelIndex = origin.panelIndex;
+		this.destination.snapPointIndex = destinationSnapPointIndex;
+		this.i = get(screens)[screenIndex].signalLines.array.length;
+		this.color.background = get(colorState).signalLine.background;
+		this.lineWidth = screen.width < screen.height ? screen.width / 20 : screen.height / 20;
+		this.setDestinationPanelIndex();
+		this.setIndexesWithInPanel();
+		// console.log(origin, destinationSnapPointIndex);
+	}
 
-  getLineWidth() {
-    let screen = get(screens)[this.screenIndex];
+	getLineWidth() {
+		let screen = get(screens)[this.screenIndex];
 
-    return screen.width < screen.height
-      ? screen.width / 20
-      : screen.height / 20;
-  }
+		return screen.width < screen.height ? screen.width / 20 : screen.height / 20;
+	}
 
-  setIndexesWithInPanel() {
-    let oSp: SnapPointObj =
-      get(screens)[this.screenIndex].snapPoints.array[
-        this.origin.snapPointIndex
-      ];
-    let o = oSp.pointIndexWithinPanel;
-    let dSp: SnapPointObj =
-      get(screens)[this.screenIndex].snapPoints.array[
-        this.destination.snapPointIndex
-      ];
-    let d = dSp.pointIndexWithinPanel;
-    this.origin.pointIndexWithinPanel = o;
-    this.destination.pointIndexWithinPanel = d;
-  }
+	setIndexesWithInPanel() {
+		let oSp: SnapPointObj =
+			get(screens)[this.screenIndex].snapPoints.array[this.origin.snapPointIndex];
+		let o = oSp.pointIndexWithinPanel;
+		let dSp: SnapPointObj =
+			get(screens)[this.screenIndex].snapPoints.array[this.destination.snapPointIndex];
+		let d = dSp.pointIndexWithinPanel;
+		this.origin.pointIndexWithinPanel = o;
+		this.destination.pointIndexWithinPanel = d;
+	}
 
-  setDestinationPanelIndex() {
-    let sp =
-      get(screens)[this.screenIndex].snapPoints.array[
-        this.destination.snapPointIndex
-      ];
-    this.destination.panelIndex = sp.panelIndex;
-  }
+	setDestinationPanelIndex() {
+		let sp = get(screens)[this.screenIndex].snapPoints.array[this.destination.snapPointIndex];
+		this.destination.panelIndex = sp.panelIndex;
+	}
 
-  setIsSelected(boolean: boolean) {
-    this.isSelected = boolean;
-  }
+	setIsSelected(boolean: boolean) {
+		this.isSelected = boolean;
+	}
 
-  setEndCoordinates(e) {
-    const panel = e.path[1].__data__;
-    const indexOfPoint = e.path[0].__data__;
-    this.destination.snapPointIndex = indexOfPoint;
-    this.destination.panelIndex = panel.i;
-  }
+	setEndCoordinates(e) {
+		const panel = e.path[1].__data__;
+		const indexOfPoint = e.path[0].__data__;
+		this.destination.snapPointIndex = indexOfPoint;
+		this.destination.panelIndex = panel.i;
+	}
 
-  updateColor(color: string) {
-    this.color.background = color;
-  }
+	updateColor(color: string) {
+		this.color.background = color;
+	}
 
-  updateLineWidth(width: number) {
-    this.lineWidth = width;
-  }
+	updateLineWidth(width: number) {
+		this.lineWidth = width;
+	}
 
-  setColor(key: ColorObjKey, color: string) {
-    this.color[key] = color;
-  }
+	setColor(key: ColorObjKey, color: string) {
+		this.color[key] = color;
+	}
 
-  getLengthInMM() {
-    const originSnapPoint: SnapPointObj =
-      get(screens)[this.screenIndex].snapPoints.array[
-        this.origin.snapPointIndex
-      ];
+	getLengthInMM() {
+		const originSnapPoint: SnapPointObj =
+			get(screens)[this.screenIndex].snapPoints.array[this.origin.snapPointIndex];
 
-    const destinationSnapPoint: SnapPointObj =
-      get(screens)[this.screenIndex].snapPoints.array[
-        this.destination.snapPointIndex
-      ];
+		const destinationSnapPoint: SnapPointObj =
+			get(screens)[this.screenIndex].snapPoints.array[this.destination.snapPointIndex];
 
-    const screen = get(screens)[this.screenIndex];
+		const screen = get(screens)[this.screenIndex];
 
-    const destinationX =
-      (destinationSnapPoint.getX() / screen.width) * screen.widthMM;
-    const destinationY =
-      (destinationSnapPoint.getY() / screen.height) * screen.heightMM;
+		const destinationX = (destinationSnapPoint.getX() / screen.width) * screen.widthMM;
+		const destinationY = (destinationSnapPoint.getY() / screen.height) * screen.heightMM;
 
-    const originX = (originSnapPoint.getX() / screen.width) * screen.widthMM;
-    const originY = (originSnapPoint.getY() / screen.height) * screen.heightMM;
+		const originX = (originSnapPoint.getX() / screen.width) * screen.widthMM;
+		const originY = (originSnapPoint.getY() / screen.height) * screen.heightMM;
 
-    const a = Math.abs(destinationX - originX);
-    const b = Math.abs(destinationY - originY);
-    const c = Math.floor(Math.sqrt(a * a + b * b) / 25) * 25;
+		const a = Math.abs(destinationX - originX);
+		const b = Math.abs(destinationY - originY);
+		const c = Math.floor(Math.sqrt(a * a + b * b) / 50) * 50;
 
-    return c;
-  }
+		return c;
+	}
 }
